380ee1d0ddeabb6d954680ccf04cbc282588e423
Juergen Pabel <juergen@pabel.net>
2022-04-26 01:34:54 +0200

    working uwsgi:kalliope for en-us

--- system/raspberry-os/bullseye/install.sh
+++ system/raspberry-os/bullseye/install.sh
@@ -23,8 +23,6 @@ systemctl restart mosquitto
 
 pip3 install -r ../../../enclosure/src/requirements.txt
 cp -r --dereference ../../../enclosure/src /opt/hal9000/enclosure
-chown -R root.hal9000 /opt/hal9000/enclosure
-chmod -R 750          /opt/hal9000/enclosure
 cat conf/uwsgi/enclosure.ini \
 	| sed 's#/data/git/HAL9000-kalliope/enclosure/src#/opt/hal9000/enclosure#g' \
 	| sed 's#/data/git/HAL9000-kalliope/resources/images#/opt/hal9000/enclosure/images#g' \
@@ -54,11 +52,17 @@ read -p "Please enter language: " BRAIN
 ln -s /opt/hal9000/kalliope/brains/"$BRAIN" /opt/hal9000/kalliope/brains/active
 
 cd /opt/hal9000/kalliope/brains/active/
-kalliope install --git-url https://github.com/kalliope-project/kalliope_trigger_porcupine.git
+kalliope install --git-url https://github.com/kalliope-project/kalliope_trigger_precise.git
 kalliope install --git-url https://github.com/juergenpabel/kalliope_stt_vosk.git
 kalliope install --git-url https://github.com/juergenpabel/kalliope_neuron_sonos.git
 
 rm -rf /tmp/kalliope
+
+chown -R root.hal9000 /opt/hal9000
+find /opt/hal9000 -type d              -exec chmod 750 {} \;
+find /opt/hal9000 -type f              -exec chmod 640 {} \;
+find /opt/hal9000 -type f -name "*.py" -exec chmod 750 {} \;
+find /opt/hal9000 -type f -name "*.sh" -exec chmod 750 {} \;
 
 echo "Please download a corresponding language model (small version) from https://alphacephei.com/vosk/models/ and extract it into /opt/hal9000/kalliope/brains/active/resources/data/vosk/"
 echo "Please verify installed configuration and reboot"
