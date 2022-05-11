b23fd0649f715b206d9abfab740bc8b2dee48f34
Juergen Pabel <juergen@pabel.net>
2022-05-11 12:28:39 +0200

    refactored MCP230XX_Rotary

--- enclosure/src/driver/mcp230xx_rotary.py
+++ enclosure/src/driver/mcp230xx_rotary.py
@@ -6,25 +6,27 @@ import busio
 import digitalio
 
 from adafruit_mcp230xx.digital_inout import DigitalInOut
+from adafruit_mcp230xx.mcp230xx import MCP230XX
 from adafruit_mcp230xx.mcp23017 import MCP23017
 
 from threading import Thread
 
 
-class MCP230XX_Rotary():
+class MCP230XX_Rotary(Thread):
 
-	def __init__(self,
-		     dio_rot_GND: DigitalInOut, dio_rot_sigA: DigitalInOut, dio_rot_sigB: DigitalInOut,
-		     dio_btn_GND: DigitalInOut = None, dio_btn_sigX: DigitalInOut = None,
-		     dio_irq: DigitalInOut = None
+	def __init__(self, mcp: MCP230XX, bank: str,
+		     rot_sigA: int, rot_gnd: int, rot_sigB: int,
+		     btn_gnd: int = 0, btn_sigX: int = 0,
+		     pin_irq = None
 		    ) -> None:
 		Thread.__init__(self)
-		self.dio_rot_GND = dio_rot_GND
-		self.dio_rot_sigA = dio_rot_sigA
-		self.dio_rot_sigB = dio_rot_sigB
-		self.dio_btn_GND = dio_btn_GND
-		self.dio_btn_sigX = dio_btn_sigX
-		self.dio_irq = dio_irq
+		self.mcp = mcp
+		self.dio_rot_sigA = mcp.get_pin(self.bank_pin(bank, rot_sigA))
+		self.dio_rot_GND = mcp.get_pin(self.bank_pin(bank, rot_gnd))
+		self.dio_rot_sigB = mcp.get_pin(self.bank_pin(bank, rot_sigB))
+		self.dio_btn_GND = mcp.get_pin(self.bank_pin(bank, btn_gnd))
+		self.dio_btn_sigX = mcp.get_pin(self.bank_pin(bank, btn_sigX))
+		self.dio_irq = digitalio.DigitalInOut(pin_irq)
 
 		for pin in [self.dio_rot_GND, self.dio_btn_GND]:
 			if pin is not None:
@@ -34,19 +36,26 @@ class MCP230XX_Rotary():
 			if pin is not None:
 				pin.direction = digitalio.Direction.INPUT
 				pin.pull = digitalio.Pull.UP
-
-		self.thread = None
+		if self.dio_irq is not None:
+			if bank == 'A':
+				mcp.interrupt_enable |= 0xFF00
+			if bank == 'B':
+				mcp.interrupt_enable |= 0x00FF
+			mcp.interrupt_configuration = 0x0000
+			mcp.io_control = 0x00
+			mcp.clear_ints()
 
 		self.rotary = dict()
 		self.rotary['pos'] = 0
 		self.rotary['min'] = 0
 		self.rotary['max'] = 100
 		self.rotary['state'] = dict()
-		self.rotary['state']['data'] = '00'
-		self.rotary['state']['direction'] = None
-
+		self.rotary['state']['data'] = 0x00
+		self.rotary['state']['direction'] = 0
 		self.button = dict()
 		self.button['status'] = True
+
+		self.thread = None
 
 
 	def configure(self, rotary_pos: int = 0, rotary_min: int = 0, rotary_max: int = 100, button_status: bool = False) -> None:
@@ -58,6 +67,8 @@ class MCP230XX_Rotary():
 
 	def do_loop(self, callback_rotary = None, callback_button = None):
 		if self.dio_irq is None or self.dio_irq.value is False:
+			if self.dio_irq is not None:
+				self.mcp.clear_ints()
 			if self.dio_rot_sigA is not None and self.dio_rot_sigB is not None:
 				rotary_pos = self.calculate_rotary(self.dio_rot_sigA.value, self.dio_rot_sigB.value)
 			if self.dio_btn_sigX is not None:
@@ -72,19 +83,14 @@ class MCP230XX_Rotary():
 					callback_button(self.status)
 
 
-	def loop(self, interval_ms: int = 1, callback_rotary = None, callback_button = None):
-		if self.thread is None:
-			self.thread = False
+	def loop(self, interval_ms: float = 1, callback_rotary = None, callback_button = None):
 		while self.thread is not None:
-			if self.thread is False:
-				self.thread = None
 			self.do_loop(callback_rotary, callback_button)
-			if self.thread is not None:
-				if interval_ms > 0:
-					time.sleep(float(interval_ms) / 1000)
+			if interval_ms > 0:
+				time.sleep(interval_ms / 1000)
 
 
-	def loop_start(self, interval_ms: int = 10, callback_rotary = None, callback_button = None) -> None:
+	def loop_start(self, interval_ms: float = 1, callback_rotary = None, callback_button = None) -> None:
 		if self.thread is None:
 			self.thread = Thread(target=self.loop, args=(interval_ms, callback_rotary, callback_button))
 			self.thread.start()
@@ -114,39 +120,53 @@ class MCP230XX_Rotary():
 
 
 	def calculate_rotary(self, valA: int, valB: int) -> int:
-		rotary_state_data = "{:d}{:d}".format(valA, valB)
+		rotary_state_data = ((valA&0x01) << 4) + ((valB&0x01) << 0)
 		if rotary_state_data != self.rotary['state']['data']:
 			rotary_pos = self.rotary['pos']
-			if self.rotary['state']['data'] == "00": # Resting position
-				if rotary_state_data == "01": # Turned right 1
-					self.rotary['state']['direction'] = "R"
-				elif rotary_state_data == "10": # Turned left 1
-					self.rotary['state']['direction'] = "L"
-			elif self.rotary['state']['data'] == "01": # R1 or L3 position
-				if rotary_state_data == "11": # Turned right 1
-					self.rotary['state']['direction'] = "R"
-				elif rotary_state_data == "00": # Turned left 1
-					if self.rotary['state']['direction'] == "L":
+			if self.rotary['state']['data'] == 0x00:
+				if rotary_state_data == 0x01:
+					self.rotary['state']['direction'] = +1
+				elif rotary_state_data == 0x10:
+					self.rotary['state']['direction'] = -1
+			elif self.rotary['state']['data'] == 0x01:
+				if rotary_state_data == 0x11:
+					self.rotary['state']['direction'] = +1
+				elif rotary_state_data == 0x00:
+					if self.rotary['state']['direction'] == -1:
 						rotary_pos = rotary_pos - 1
-			elif self.rotary['state']['data'] == "10": # R3 or L1
-				if rotary_state_data == "11": # Turned left 1
-					self.rotary['state']['direction'] = "L"
-				elif rotary_state_data == "00": # Turned right 1
-					if self.rotary['state']['direction'] == "R":
+			elif self.rotary['state']['data'] == 0x10:
+				if rotary_state_data == 0x11:
+					self.rotary['state']['direction'] = -1
+				elif rotary_state_data == 0x00:
+					if self.rotary['state']['direction'] == +1:
 						rotary_pos = rotary_pos + 1
-			else: # self.rotary['state']['data'] == "11"
-				if rotary_state_data == "01": # Turned left 1
-					self.rotary['state']['direction'] = "L"
-				elif rotary_state_data == "10": # Turned right 1
-					self.rotary['state']['direction'] = "R"
-				elif rotary_state_data == "00": # Skipped an intermediate 01 or 10 state, but if we know direction then a turn is complete
-					if self.rotary['state']['direction'] == "L":
+			else:
+				if rotary_state_data == 0x01:
+					self.rotary['state']['direction'] = -1
+				elif rotary_state_data == 0x10:
+					self.rotary['state']['direction'] = +1
+				elif rotary_state_data == 0x00:
+					if self.rotary['state']['direction'] == -1:
 						rotary_pos = rotary_pos - 1
-					elif self.rotary['state']['direction'] == "R":
+					elif self.rotary['state']['direction'] == +1:
 						rotary_pos = rotary_pos + 1
 			self.rotary['state']['data'] = rotary_state_data
 			return rotary_pos
 		return self.rotary['pos']
+
+	def bank_pin(self, bank: str, pin: int) -> int:
+		if bank == 'A':
+			return self.bank_a_pin(pin)
+		if bank == 'B':
+			return self.bank_b_pin(pin)
+		raise Exception("MCP230xx bank must be either 'A' or 'B'")
+
+	def bank_a_pin(self, pin: int) -> int:
+		return (pin + 0) & 0x07
+
+
+	def bank_b_pin(self, pin: int) -> int:
+		return (pin + 8) & 0x0f
 
 
 
@@ -160,31 +180,14 @@ def print_button(position: int):
 
 i2c = busio.I2C(board.SCL, board.SDA)
 mcp = MCP23017(i2c, address=0x20)
-
-pin_rot_sigA = mcp.get_pin(5)
-pin_rot_gnd = mcp.get_pin(6)
-pin_rot_sigB = mcp.get_pin(7)
-pin_btn_gnd = mcp.get_pin(3)
-pin_btn_sigX = mcp.get_pin(4)
-mcp230xx_a = MCP230XX_Rotary(pin_rot_gnd, pin_rot_sigA, pin_rot_sigB, pin_btn_gnd, pin_btn_sigX)
-mcp230xx_a.configure(50, 0, 100, False)
-
-pin_rot_sigA = mcp.get_pin(10)
-pin_rot_gnd = mcp.get_pin(9)
-pin_rot_sigB = mcp.get_pin(8)
-pin_btn_gnd = mcp.get_pin(12)
-pin_btn_sigX = mcp.get_pin(11)
-mcp230xx_b = MCP230XX_Rotary(pin_rot_gnd, pin_rot_sigA, pin_rot_sigB, pin_btn_gnd, pin_btn_sigX)
-mcp230xx_b.configure(50, 0, 100, False)
-
-irq_a = digitalio.DigitalInOut(board.D12)
-irq_b = digitalio.DigitalInOut(board.D13)
+mcp230xx_a = MCP230XX_Rotary(mcp, 'A', 5, 6, 7, 3, 4, board.D12)
+mcp230xx_b = MCP230XX_Rotary(mcp, 'B', 0, 1, 2, 3, 4, board.D13)
+mcp230xx_a.configure(5, 0, 10, False)
+mcp230xx_b.configure(5, 0, 10, False)
+mcp230xx_a.loop_start(2.5, print_rotary, print_button)
+mcp230xx_b.loop_start(2.5, print_rotary, print_button)
 while True:
-	if not irq_a.value:
-		mcp.clear_inta()
-		mcp230xx_a.do_loop(print_rotary, print_button)
-	if not irq_b.value:
-		mcp.clear_intb()
-		mcp230xx_b.do_loop(print_rotary, print_button)
-	time.sleep(0.001)
+#	mcp230xx_a.do_loop(print_rotary, print_button)
+#	mcp230xx_b.do_loop(print_rotary, print_button)
+	time.sleep(0.0025)
 
