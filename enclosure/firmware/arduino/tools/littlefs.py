5268f75a53f8bec72ecc8819c6254c3cf77633e2
Juergen Pabel <juergen@pabel.net>
2024-07-16 10:59:29 +0200

    firmware/arduino: reduced images (quality and frames) for 1.75MB littlefs target-size

--- enclosure/firmware/arduino/tools/littlefs.py
+++ enclosure/firmware/arduino/tools/littlefs.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-from shutil import copytree as shutil_copytree
+from shutil import copytree as shutil_copytree, \
+                   ignore_patterns as shutil_ignore_patterns
 
-shutil_copytree('./resources/images/frontend/animations', './data/images/animations', dirs_exist_ok=True)
-shutil_copytree('./resources/images/frontend/overlays',   './data/images/overlays', dirs_exist_ok=True)
-shutil_copytree('./resources/system',                     './data/system', dirs_exist_ok=True)
+shutil_copytree('./resources/images/frontend/animations', './data/images/animations', dirs_exist_ok=True, ignore=shutil_ignore_patterns('README.md', 'LICENSE'))
+shutil_copytree('./resources/images/frontend/overlays',   './data/images/overlays', dirs_exist_ok=True, ignore=shutil_ignore_patterns('README.md', 'LICENSE'))
+shutil_copytree('./resources/system',                     './data/system', dirs_exist_ok=True, ignore=shutil_ignore_patterns('README.md', 'LICENSE'))
 
