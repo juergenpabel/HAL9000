6a0954279648d679ac9117afbb701a35d8e02280
Juergen Pabel <juergen@pabel.net>
2024-08-01 16:19:31 +0200

    firmware: removed overlays dir from littlefs generator script

--- enclosure/firmware/arduino/tools/littlefs.py
+++ enclosure/firmware/arduino/tools/littlefs.py
@@ -4,6 +4,5 @@ from shutil import copytree as shutil_copytree, \
                    ignore_patterns as shutil_ignore_patterns
 
 shutil_copytree('./resources/images/frontend/animations', './data/images/animations', dirs_exist_ok=True, ignore=shutil_ignore_patterns('README.md', 'LICENSE'))
-shutil_copytree('./resources/images/frontend/overlays',   './data/images/overlays', dirs_exist_ok=True, ignore=shutil_ignore_patterns('README.md', 'LICENSE'))
 shutil_copytree('./resources/system',                     './data/system', dirs_exist_ok=True, ignore=shutil_ignore_patterns('README.md', 'LICENSE'))
 
