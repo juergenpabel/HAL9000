59771ace04263c8b8de3cadefffb379cd7d9d535
Juergen Pabel <juergen@pabel.net>
2024-10-05 01:32:51 +0200

    console: minor code changes

--- enclosure/services/console/console.py
+++ enclosure/services/console/console.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
 
-import sys
-import os
-import os.path
-import flet_core
-import flet_core.border
-import flet_core.colors
+from sys import exit as sys_exit
+from os import getcwd as os_getcwd
+from os.path import exists as os_path_exists, \
+                    islink as os_path_islink, \
+                    realpath as os_path_realpath
+
+from flet import Page as flet_Page, \
+                 Row as flet_Row ,\
+                 Column as flet_Column ,\
+                 ThemeMode as flet_ThemeMode, \
 import flet
-import flet.fastapi as flet_fastapi
+from flet.fastapi import app as flet_fastapi_app
 from uvicorn import run as uvicorn_run
+from uvicorn.config import LOGGING_CONFIG as uvicorn_config_LOGGING_CONFIG
 from fastapi import FastAPI as fastapi_FastAPI
 from fastapi.staticfiles import StaticFiles as fastapi_staticfiles_StaticFiles
 from logging import getLogger as logging_getLogger, \
@@ -45,17 +50,17 @@ SCREENS = {
 }
 
 
-def console(page: flet.Page):
+def console(page: flet_Page):
 	page.title = "HAL9000 Console"
-	page.theme_mode = flet.ThemeMode.DARK
+	page.theme_mode = flet_ThemeMode.DARK
 
-	menu_lo = flet.Column(width=150)
-	menu_li = flet.Column(width=150)
-	menu_hal = flet.Column(width=110, controls=[flet.Container(content=flet.Image(src="/assets/images/HAL9000.jpg", fit=flet.ImageFit.FILL),
+	menu_lo = flet_Column(width=150)
+	menu_li = flet_Column(width=150)
+	menu_hal = flet_Column(width=110, controls=[flet.Container(content=flet.Image(src="/assets/images/HAL9000.jpg", fit=flet.ImageFit.FILL),
 	                                                           on_click=SCREENS["start"]["screen"].on_show)],
 	                       horizontal_alignment=flet.CrossAxisAlignment.CENTER)
-	menu_ri = flet.Column(width=150)
-	menu_ro = flet.Column(width=150)
+	menu_ri = flet_Column(width=150)
+	menu_ro = flet_Column(width=150)
 	parent = flet.AnimatedSwitcher(width=750, height=400, content=SCREENS["start"]["screen"],
 	                               transition=flet.AnimatedSwitcherTransition.FADE,
 	                               switch_in_curve=flet.AnimationCurve.EASE_OUT,
@@ -70,29 +75,30 @@ def console(page: flet.Page):
 				                                                 fit=flet.ImageFit.FILL,
 				                                                 border_radius=flet.border_radius.all(5)),
 				                              on_click=SCREENS[subsystem][module]["screen"].on_show))
-	page.add(flet.Row(controls=[menu_lo, menu_li, menu_hal, menu_ri, menu_ro], alignment=flet.MainAxisAlignment.CENTER))
-	page.add(flet.Row(controls=[flet.Container(border=flet.border.all(1, flet.colors.GREY), border_radius=flet.border_radius.all(10),
+	page.add(flet_Row(controls=[menu_lo, menu_li, menu_hal, menu_ri, menu_ro], alignment=flet.MainAxisAlignment.CENTER))
+	page.add(flet_Row(controls=[flet.Container(border=flet.border.all(1, flet.colors.GREY), border_radius=flet.border_radius.all(10),
 	                                           bgcolor=flet.colors.BLACK, content=parent)], alignment=flet.MainAxisAlignment.CENTER))
 	page.update()
 
 
 async def fastapi_lifespan(app: fastapi_FastAPI):
-	assets_dir = f'{os.getcwd()}/assets'
-	if os.path.islink(assets_dir) is True:
-		assets_dir = os.path.realpath(assets_dir)
+	assets_dir = f'{os_getcwd()}/assets'
+	if os_path_islink(assets_dir) is True:
+		assets_dir = os_path_realpath(assets_dir)
 	app.mount('/assets/', fastapi_staticfiles_StaticFiles(directory=assets_dir, follow_symlink=True), name="assets")
-	app.mount('/',       flet_fastapi.app(console, route_url_strategy='path'))
+	app.mount('/',       flet_fastapi_app(console, route_url_strategy='path'))
 	yield
 
 
 app = fastapi_FastAPI(lifespan=fastapi_lifespan)
 if __name__ == '__main__':
 	logging_addLevelName(5, 'TRACE')
-	if os.path.exists('assets') is False:
+	if os_path_exists('assets') is False:
 		logging_getLogger().critical("[console] missing 'assets' directory (or symlink to directory)")
 		sys_exit(1)
 	logging_getLogger().info("[console] starting...")
 	try:
+		uvicorn_config_LOGGING_CONFIG["formatters"]["default"]["fmt"] = "%(asctime)s %(levelprefix)s %(message)s"
 		uvicorn_run('console:app', host='0.0.0.0', port=2001, log_level='info')
 	except KeyboardInterrupt:
 		logging_getLogger().info("[console] exiting due to CTRL-C")
